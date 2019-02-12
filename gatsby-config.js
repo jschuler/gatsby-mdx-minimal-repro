0577a9fca03f0198f035705e773a7c4b38b2c5d6
Joachim Schuler <jschuler@redhat.com>
2019-02-12 10:00:21 -0500

    collision issue with gatsby-transformer-react-docgen

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,13 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'components',
+        path: `${__dirname}/components/`,
+      },
+    },
+    'gatsby-transformer-react-docgen',
   ],
 }
